17fa4b5f2a9e6d34478da62ecf8ccddde615ddfb
zxc7563598 <junjie.he.925@gmail.com>
2025-06-08 16:49:07 +0800

    feat(page): 用户管理增加价格类型

--- src/views/shop-management/user-management/api.js
+++ src/views/shop-management/user-management/api.js
@@ -6,6 +6,8 @@ export default {
   getUserInfo: uid => request.post('/shop-management/user-management/get-user-info', { uid }), // 根据UID查询用户数据
   setData: (user_id, uid, name, password, vip_type) => request.post('/shop-management/user-management/set-data', { user_id, uid, name, password, vip_type }, { removeEmpty: false }), // 存储用户信息
   resetPassword: () => request.post('/shop-management/user-management/reset-password'), // 清空所有用户密码
-  getUserRecords: user_id => request.post('/shop-management/user-management/get-user-records', { user_id }), // 获取用户航海开通记录
+  getUserPointRecords: user_id => request.post('/shop-management/user-management/get-user-point-records', { user_id }), // 获取用户积分变更记录
+  getUserCoinRecords: user_id => request.post('/shop-management/user-management/get-user-coin-records', { user_id }), // 获取用户硬币变更记录
   setUserPoint: (type, point, user_id) => request.post('/shop-management/user-management/set-user-point', { type, point, user_id }), // 变更用户积分
+  setUserCoin: (type, coin, user_id) => request.post('/shop-management/user-management/set-user-coin', { type, coin, user_id }), // 变更用户硬币
 }
