44cddd5b5473371e000e420c1f94ef3e06856f45
zxc7563598 <junjie.he.925@gmail.com>
2025-10-22 09:22:46 +0800

    refactor(page): 去除系统配置中无效的配置

--- src/views/configuration/system-settings/api.js
+++ src/views/configuration/system-settings/api.js
@@ -3,5 +3,5 @@ import { request } from '@/utils'
 export default {
   getData: () => request.post('/configuration/system-settings/get-data'), // 获取配置
   getDataQrcode: url => request.post('/configuration/system-settings/get-data-qrcode', { url }), // 获取二维码
-  setData: (shop_name, shop_url, system_api_url, system_aes_key, system_aes_iv, system_key, host, listen, re_open_host, secure_api_key, redis_host, redis_port, redis_password, db_host, db_port, db_name, db_user, db_password) => request.post('/configuration/system-settings/set-data', { shop_name, shop_url, system_api_url, system_aes_key, system_aes_iv, system_key, host, listen, re_open_host, secure_api_key, redis_host, redis_port, redis_password, db_host, db_port, db_name, db_user, db_password }, { removeEmpty: false }), // 存储数据
+  setData: (shop_name, shop_url, system_api_url, host, listen, re_open_host, secure_api_key, redis_host, redis_port, redis_password, db_host, db_port, db_name, db_user, db_password) => request.post('/configuration/system-settings/set-data', { shop_name, shop_url, system_api_url, host, listen, re_open_host, secure_api_key, redis_host, redis_port, redis_password, db_host, db_port, db_name, db_user, db_password }, { removeEmpty: false }), // 存储数据
 }
